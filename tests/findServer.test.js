aadaa274582a5dc2987ffda04e6c0d0839283525
Mostafa Khalid <mostafakhalid0996@gmail.com>
2019-10-18 20:14:08 +0200

    Add test cases

--- tests/findServer.test.js
+++ tests/findServer.test.js
@@ -7,43 +7,116 @@ chai.use(request);
 chai.should();
 
 describe('#POST /bosta/check-server', function() {
-	this.timeout(10000); // this test can take up to 5 seconds
+	this.timeout(10000);
+	let response = {}
+	describe('# Make a post request with non-online server', () => {
+		it('* Get response from the post request', done => {
+			chai
+				.request(app)
+				.post('/bosta/check-server')
+				.set('Content-Type', 'application/json')
+				.send({
+					serverArray: [
+						{
+							url: 'https://github.com/mostafa0996/nodej.js',
+							priority: 1,
+						}
+					],
+				})
+				.end((err, res) => {
+					if (!err) response = res;
+					done();
+				});
+		});
+	
+		it('* Check status code to be 200' ,  done => {
+			expect(response.status).to.equal(200);
+			done();
+		});
+	
+		it('* Check body of response ' ,  done => {
+			expect(response.body).to.have.property('err','No online servers');
+			done();
+		});
+	})
 
-	it('* Make A get Request and get a response', done => {
-		chai
-			.request(app)
-			.post('/bosta/check-server')
-			.set('Content-Type', 'application/json')
-			.send({
-				serverArray: [
-					{
-						url: 'https://github.com/mostafa0996/nodej.js',
-						priority: 1,
-					},
-					// {
-					// 	url: 'http://bosta.co',
-					// 	priority: 7,
-					// },
-					// {
-					// 	url: 'http://offline.bosta.co',
-					// 	priority: 2,
-					// },
-					// {
-					// 	url: 'http://google.com',
-					// 	priority: 4,
-					// },
-					// {
-					// 	url: 'http://www.medium.com',
-					// 	priority: 3,
-					// },
-				],
-			})
-			.end((err, res) => {
-				if (err) done(err);
-				console.log(res.body);
-				res.should.have.status(200);
-				res.body.should.be.a('object');
-			});
-		done();
+	describe('# Make a post request with non-online and online servers ', () => {
+		it('* Get response from the post request', done => {
+			chai
+				.request(app)
+				.post('/bosta/check-server')
+				.set('Content-Type', 'application/json')
+				.send({
+					serverArray: [
+						{
+							url: 'https://github.com/mostafa0996/nodej.js',
+							priority: 1,
+						},		     	
+						{
+							url: 'http://bosta.co',
+							priority: 7
+						},
+					],
+				})
+				.end((err, res) => {
+					if (!err) response = res;
+					done();
+				});
+		});
+	
+		it('* Check status code to be 200' ,  done => {
+			expect(response.status).to.equal(200);
+			done();
+		});
+
+		it('* Check response body data' ,  done => {
+			expect(response.body).to.have.property('url','https://bosta.co/');
+			expect(response.body).to.have.property('priority',7);
+			done();
+		});
+	});
+
+	describe('# Make a post request with non-online and online servers and online server that took more than 5 secs', () => {
+		it('* Get response from the post request', done => {
+			chai
+				.request(app)
+				.post('/bosta/check-server')
+				.set('Content-Type', 'application/json')
+				.send({
+					serverArray: [
+						{
+							url: 'https://github.com/mostafa0996/nodej.js',
+							priority: 1,
+						},		     	
+						{
+							url: 'http://bosta.co',
+							priority: 7
+						},
+						{
+							url: 'http://www.medium.com',
+							priority: 3
+						},
+						{
+							url: 'http://offline.bosta.co',
+							priority: 2
+						},
+					],
+				})
+				.end((err, res) => {
+					if (!err) response = res;
+					done();
+				});
+		});
+	
+		it('* Check status code to be 200' ,  done => {
+			expect(response.status).to.equal(200);
+			done();
+		});
+
+		it('* Check response body data' ,  done => {
+			expect(response.body).to.have.property('url','http://offline.bosta.co/');
+			expect(response.body).to.have.property('priority',2);
+			done();
+		});
 	});
 });
